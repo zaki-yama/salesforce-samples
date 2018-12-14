b06bdec4d5b7b7859a2f9ae42de4f67d563cd7f9
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-14 19:47:10 +0900

    Implement helloWorld lwc

--- trailhead-quick-start-lightning-web-components/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ trailhead-quick-start-lightning-web-components/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+  @track greeting = 'World';
+  changeHandler(event) {
+    this.greeting = event.target.value;
+  }
+}
