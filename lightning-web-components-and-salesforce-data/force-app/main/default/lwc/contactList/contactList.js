c834f717d2021607c90cecbce132a37f0f338711
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-01-28 22:34:07 +0900

    Challenge 2

---
+++ lightning-web-components-and-salesforce-data/force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,20 @@
+import { LightningElement, wire } from "lwc";
+import FIRST_NAME_FIELD from "@salesforce/schema/Contact.FirstName";
+import LAST_NAME_FIELD from "@salesforce/schema/Contact.LastName";
+import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
+import getContacts from "@salesforce/apex/ContactController.getContacts";
+const COLUMNS = [
+  {
+    label: "First Name",
+    fieldName: FIRST_NAME_FIELD.fieldApiName,
+    type: "text"
+  },
+  { label: "Last Name", fieldName: LAST_NAME_FIELD.fieldApiName, type: "text" },
+  { label: "Email", fieldName: EMAIL_FIELD.fieldApiName, type: "email" }
+];
+
+export default class ContactList extends LightningElement {
+  columns = COLUMNS;
+  @wire(getContacts)
+  contacts;
+}
