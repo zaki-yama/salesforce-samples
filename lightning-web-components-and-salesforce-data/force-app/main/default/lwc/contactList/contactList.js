f998c237f5e44e4638b246d611b78c0b0cd891f9
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-01-28 22:39:02 +0900

    Challenge 3

--- lightning-web-components-and-salesforce-data/force-app/main/default/lwc/contactList/contactList.js
+++ lightning-web-components-and-salesforce-data/force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from "lwc";
+import { reduceErrors } from "c/ldsUtils";
 import FIRST_NAME_FIELD from "@salesforce/schema/Contact.FirstName";
 import LAST_NAME_FIELD from "@salesforce/schema/Contact.LastName";
 import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
@@ -17,4 +18,8 @@ export default class ContactList extends LightningElement {
   columns = COLUMNS;
   @wire(getContacts)
   contacts;
+
+  get errors() {
+    return this.contacts.error ? reduceErrors(this.contacts.error) : [];
+  }
 }
