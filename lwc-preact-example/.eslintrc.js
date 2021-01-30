746aff958f8c0e602757088bcc3ae9ce9e319c7d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-01-31 00:31:04 +0900

    deps: @cybozu/eslint-config

---
+++ lwc-preact-example/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: "@cybozu/eslint-config/presets/react",
+  parser: "babel-eslint",
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": "off"
+  }
+};
