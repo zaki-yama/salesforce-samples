4951c1c8b5b9d14ac8e6849282d1a9055ad65efe
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-03-29 12:49:26 +0900

    DOMの非同期更新のテスト

--- trailhead/test-lightning-web-components/force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ trailhead/test-lightning-web-components/force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -19,4 +19,37 @@ describe("c-unit-test", () => {
     const div = element.shadowRoot.querySelector("div");
     expect(div.textContent).toBe("Unit 5 alive!");
   });
+
+  it("displays unit status with updated unitNumber", () => {
+    const element = createElement("c-unit-test", {
+      is: UnitTest
+    });
+    // Add the element to the jsdom instance
+    document.body.appendChild(element);
+    // Update unitNumber after element is appended
+    element.unitNumber = 6;
+
+    const div = element.shadowRoot.querySelector("div");
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe("Unit 6 alive!");
+    });
+  });
+
+  it("displays unit status with input change event", () => {
+    const element = createElement("c-unit-test", {
+      is: UnitTest
+    });
+    // Add the element to the jsdom instance
+    document.body.appendChild(element);
+    const div = element.shadowRoot.querySelector("div");
+
+    // Trigger unit status input change
+    const inputElement = element.shadowRoot.querySelector("lightning-input");
+    inputElement.value = 7;
+    inputElement.dispatchEvent(new CustomEvent("change"));
+
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe("Unit 7 alive!");
+    });
+  });
 });
--- trailhead/test-lightning-web-components/force-app/main/default/lwc/unitTest/unitTest.js
+++ trailhead/test-lightning-web-components/force-app/main/default/lwc/unitTest/unitTest.js
@@ -4,4 +4,9 @@ import { sum } from "./sum";
 export default class UnitTest extends LightningElement {
   @api
   unitNumber = sum(2, 3);
+
+  handleChange(event) {
+    // eslint-disable-next-line @lwc/lwc/no-api-reassignments
+    this.unitNumber = event.target.value;
+  }
 }
